0ac501ae7062edff10d40d6ef4e73e007655bbfe
Daniel Cabral <danicabral15@outlook.com>
2025-07-04 14:32:43 -0300

    feat: add authentication pages and components for login and registration

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { RoomPage } from "./pages/RoomPage";
+import { LoginPage } from "../features/auth/pages/LoginPage";
+import { RegisterPage } from "../features/auth/pages/RegisterPage";
+import { AuthLayout } from "../components/layouts/AuthLayout";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+
         <Route index element={<HomePage />} />
         <Route path="room/:id" element={<RoomPage />} />
       </Routes>
