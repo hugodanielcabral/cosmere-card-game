2e83f6ec555785322a9007588cb148024044c4b3
Daniel Cabral <danicabral15@outlook.com>
2025-07-10 16:39:06 -0300

    feat: implement LoginForm component with validation and error handling

---
+++ apps/frontend/src/features/auth/components/login/LoginForm.tsx
@@ -0,0 +1,134 @@
+import { useState, useTransition } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useAuth } from "../../../../hooks/useAuth";
+import { notify } from "../../../../utils/notify";
+import { AuthMessage } from "../message/AuthMessage";
+import { Form } from "../../../../components/form/Form";
+import { Label } from "../../../../components/label/Label";
+import { MdEmail, MdPassword } from "react-icons/md";
+import { Input } from "../../../../components/input/Input";
+import { Button } from "../../../../components/button/Button";
+import { Link } from "react-router";
+
+export interface ILoginInput {
+  email: string;
+  password: string;
+}
+
+const INITIAL_FORM_DATA = {
+  email: "",
+  password: "",
+};
+
+export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ILoginInput>({ defaultValues: INITIAL_FORM_DATA });
+  const [isPending, startTransition] = useTransition();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const { signin, authError, setAuthError } = useAuth(); // server errors
+
+  const onSubmit: SubmitHandler<ILoginInput> = (data) => {
+      startTransition(async () => {
+        setAuthError(null);
+        const result = await signin(data);
+        if (result) {
+          setIsSuccess(true);
+          reset();
+        } else {
+          notify("Login failed.", "error");
+        }
+      });
+    };
+
+  return (
+    <>
+      {isSuccess ? (
+          <AuthMessage
+            title="Welcome back!"
+            message="Logged in successfully!!"
+            countdownStart={5}
+            navigateTo="/"
+            buttonText="Go home now."
+            pageName="home"
+          />
+      ) : (
+        <Form
+          className="flex flex-col justify-center items-center max-w-72 gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {authError && (
+            <div className="alert alert-error mb-4">{authError?.message}</div>
+          )}
+          <Label
+            className={
+              errors.email || authError ? "border-error" : ""
+            }
+          >
+            <MdEmail color="#666666"/>
+            <Input
+              type="email"
+              placeholder="email@site.com"
+              title="Only valid email providers"
+              {...register("email", {
+                required: "Email is required.",
+                pattern: {
+                  value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/i,
+                  message:
+                    "Please enter a valid email address (must include '@' and a domain).",
+                },
+              })}
+            />
+          </Label>
+          {errors?.email && (
+            <p className="text-error text-sm">{errors.email.message}</p>
+          )}
+          <Label className={errors.password || authError ? "border-error" : ""}>
+            <MdPassword color="#666666" />
+            <Input
+              type="password"
+              placeholder="Password"
+              title="Only letters, numbers or dash"
+              {...register("password", {
+                required: "Password is required.",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long.",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Password cannot exceed 20 characters.",
+                },
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d).+$/i,
+                  message:
+                    "Password must contain at least one letter and one number.",
+                },
+              })}
+            />
+          </Label>
+          {errors?.password && (
+            <p className="text-error text-sm">{errors.password.message}</p>
+          )}
+
+          <Button
+            className="btn-primary font-bold w-full"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? "Just a moment, logging you in..." : "Login"}
+          </Button>
+          <span className="flex gap-1">
+            <p className="">Don't have an account?</p>{" "}
+            <Link className="text-warning font-bold" to="/register">
+              Register now.
+            </Link>
+          </span>
+        </Form>
+      )}
+    </>
+  );;
+};
--- apps/frontend/src/features/auth/pages/LoginPage.tsx
+++ apps/frontend/src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,12 @@
+import { LoginForm } from "../components/login/LoginForm";
+
 export const LoginPage = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="hero bg-linear-to-b from-base-100 to-base-300 min-h-screen flex flex-col">
+      <h1 className="text-3xl sm:text-4xl text-primary font-bold my-10 sm:my-12 md:my-14 lg:my-16">
+        Cosmere Card Game
+      </h1>
+      <LoginForm />
+    </div>
+  );
+};
